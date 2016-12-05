977ab406f7b2192a9c1196b6f5fa3ee05ce89f41
Mark Stillwell <mark@stillwell.me>
2016-12-05 15:18:42 +0000

    persist the attempts to login a bit more.

--- fusiondirectory-first-login.sh
+++ fusiondirectory-first-login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for X in {1..10}; do
+for X in {1..30}; do
   while ! CURLOUT=$(curl -s -X POST -F 'username=fd-admin' -F 'password=fusion' -F 'login=Sign in' http://localhost/fusiondirectory/) || [ -n "$CURLOUT" ]; do sleep 1; done
   sleep 1
 done &
